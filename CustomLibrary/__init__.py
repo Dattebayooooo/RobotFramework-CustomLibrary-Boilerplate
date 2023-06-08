28eec5257f591a051549ce32ada392aa9d937c4e
Sourab Dolai <40881051+Dattebayooooo@users.noreply.github.com>
2023-06-08 10:34:51 +0530

    Changes as per pylint

--- CustomLibrary/__init__.py
+++ CustomLibrary/__init__.py
@@ -4,18 +4,28 @@ from robot.api import logger
 
 __version__ = 'Intial'
 
+
 class CustomLibrary:
-    """This is CustomLibrary.
-    Add Library Documentation here for usage.
-    """
+    """This is CustomLibrary. Add Library Documentation here for usage."""
     
     ROBOT_LIBRARY_SCOPE = "GLOBAL"
     ROBOT_LIBRARY_DOC_FORMAT = "ROBOT"
     
     def __init__(self):
+        """Constructor not initialized"""
         pass
     
-    def test_keyword(self, argument):
-        """"""
+    def test_keyword(self, argument=None):
+        """Test Keyword Accepting optional arguments"""
         logger.info("Runing Keyword")
         logger.info("argument: {}".format(argument))
+    
+    def test_keyword2(self):
+        """Dummy keyword with no arguments"""
+        ## Add your code logic here
+        pass
+    
+    def _private_keyword(self):
+        """Private method which will not be displayed/Called in IDE"""
+        ## Add your code logic here
+        pass
